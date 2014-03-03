15c4685d609c0faa7dd48529597b85eea252439e
Jonathan Helmus <jjhelmus@gmail.com>
2014-03-03 16:58:33 -0600

    BUG: fixed setup.py so install works

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setup(name = "lrose_solopy",
     author = "Xin Pan and Joe VanAndel",
     author_email = "vanandel@ucar.edu",
     url = "https://github.com/ncareol/lrose-soloPy",
-    license = 'BSD'
+    license = 'BSD',
     #Name the folder where your packages live:
     #(If you have other packages (dirs) or modules (py files) then
-    #put them into the package directory - they will be found 
+    #put them into the package directory - they will be found
     #recursively.)
     packages = ['lrose_solopy'],
     #'package' package must contain files (see list above)
@@ -23,8 +23,8 @@ setup(name = "lrose_solopy",
     package_data = {'lrose_solopy' : files },
     #'start_pysolo' is in the root.
     scripts = ["run_solopy"],
-    long_description = """Really long text here.""" 
+    long_description = """Really long text here."""
     #
     #This next part it for the Cheese Shop, look a little down the page.
-    #classifiers = []     
-) 
+    #classifiers = []
+)
